0f70d11a6c1a2374d82f697af77fb14c1470a9ad
Boogy83 <bozhidar.atanasov@abv.bg>
2020-03-02 00:59:32 +0200

    ...

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <HelloWorld />
     <Footer />
   </div>
--- my-app/src/components/footer/footer.spec.js
+++ my-app/src/components/footer/footer.spec.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import FooterComponent from './index.vue';
+import { shallowMount } from '@vue/test-utils';
+
+const wrapper = shallowMount(FooterComponent)
 
 // Here are some Jasmine 2.0 tests, though you can
 // use any test runner / assertion library combo you prefer
