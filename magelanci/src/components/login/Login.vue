4ac4ebdf3f4ea6c56e690d87f720bef10f990a31
Boogy83 <bozhidar.atanasov@abv.bg>
2020-04-05 14:04:13 +0300

    login component added and some styles improved

---
+++ magelanci/src/components/login/Login.vue
@@ -0,0 +1,3 @@
+<template src="./app-login.html"></template>
+<script src="./app-login.ts"></script>
+<style src="./app-login.scss" scoped lang="scss"></style>
--- magelanci/src/router/index.ts
+++ magelanci/src/router/index.ts
@@ -1,29 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Register from '../components/register/Register.vue'
+import Login from '../components/login/Login.vue'
 import Home from '../components/home/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
     path: '/Home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
