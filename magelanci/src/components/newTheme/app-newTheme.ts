266cc327c62b4fcd9c36d7f33d1be48e9310e5c7
Boogy83 <bozhidar.atanasov@abv.bg>
2020-04-09 11:24:35 +0300

    new theme post-mixin implemented

--- magelanci/src/components/newTheme/app-newTheme.ts
+++ magelanci/src/components/newTheme/app-newTheme.ts
@@ -1,16 +1,43 @@
+import { required } from 'vuelidate/lib/validators'
+import newThemeMixin from '@/mixins/new-theme-mixin';
+
 export default {
-    name: 'NewTheme',
-    components: {},
-    props: [],
-    data () {
-      return {
-  
-      }
-    },
-    computed: {
-  
-    },
-    methods: {
-  
+  name: 'NewTheme',
+  components: {},
+  props: [],
+  data() {
+    return {
+      title: '',
+      content: '',
     }
-  }
+  },
+  validations: {
+    title: {
+      required,
+    },
+    content: {
+      required,
+    },
+  },
+  computed: {
+  },
+  methods: {
+    publish() {
+      (this as any).$v.$touch();
+      const payload = {
+        title: (this as any).title,
+        content: (this as any).content,
+      };
+      (this as any).postTheme(payload)
+      .then((res: any)=>{
+
+        console.log(res);
+      })
+
+    },
+    clear() {
+      console.log('clear');
+    }
+  },
+  mixins: [newThemeMixin]
+}
---
+++ magelanci/src/mixins/new-theme-mixin.ts
@@ -0,0 +1,21 @@
+import axiosDb from '@/axios-database';
+
+export default {
+    data: function () {
+        return {
+
+        }
+    },
+    methods: {
+        async postTheme(payload: {}) {
+            try {
+                const res = await axiosDb.post(`themes.json`, payload);
+                const posted = res.data.name;
+                console.log(posted);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+}
